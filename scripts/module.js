45c479bf02cbb78868747b9d834fab6f6179ac77
hakimassouane2 <hakimassouane@hotmail.com>
2025-04-30 18:34:47 +0900

    basic reset

---
+++ scripts/module.js
@@ -0,0 +1,7 @@
+Hooks.once("init", async function () {
+  console.log("S3 Music Player | Initializing");
+});
+
+Hooks.once("ready", async function () {
+  console.log("S3 Music Player | Ready");
+});
